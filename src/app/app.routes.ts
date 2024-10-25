cf5a3d33be46cf25115c770ae5825ee029d653d0
Tryphene <konetryphene@yahoo.fr>
2024-10-25 19:46:11 +0000

    contact_management_apps

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [];
+// 
+// ng add @ngrx/effects@latest
+// ng add @ngrx/store-devtools@latest
+// ng add @ngrx/entity@lates
