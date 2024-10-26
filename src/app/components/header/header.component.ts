c542b8c5a00334b2ad077b3b1679d7850f312778
Tryphene <konetryphene@yahoo.fr>
2024-10-26 19:37:50 +0000

    update

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,19 +1,4 @@
-// import { Component } from '@angular/core';
-
-// @Component({
-//   selector: 'app-header',
-//   standalone: true,
-//   imports: [],
-//   templateUrl: './header.component.html',
-//   styleUrl: './header.component.css'
-// })
-// export class HeaderComponent {
-
-// }
-
-import { Component, Input, Output } from '@angular/core';
-import { trigger, state, style, transition, animate } from '@angular/animations';
-// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Component,} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
@@ -23,22 +8,11 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   imports: [ CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
-  animations: [
-  trigger('simpleFade', [
-    state('visible', style({ opacity: 1 })),
-    state('hidden', style({ opacity: 0 })),
-    transition('visible <=> hidden', [
-      animate('1s')
-    ])
-  ])
-]
+  
 
 })
 export class HeaderComponent {
   isVisible = true;
-  @Input() title: any;
-  links: string[] = ["Accueil", "Liste"]
-  
 
   toggle() {
     this.isVisible = !this.isVisible;
